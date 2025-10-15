e14dfc2778ada635368cababd5a579812b2a760e
Your Name <your.email@example.com>
2025-10-15 14:23:41 +0500

    7-dasrs-4

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// const { Children } = require("react");
+
+
+
+// let chldren1 = document.querySelector(".chldren1");
+
+// console.log(Children1);
+
+// const btn = document.querySelector("button");
+
+// const forEvent = () => {
+//     console.log("button bosildi");
+  
+// }
+
+// btn.addEventListener("click",forEvent);
+
+// const contanier = document.querySelector(".contanier");
+// const btn = document.getElementById("btn");
+
+// btn.addEventListener("click", () => {
+//     let div = document.createElement("div");
+//     div.classList.add("list");
+//     div.textContent = "bugun dam olish kiuni";
+//     contanier.append(div)
+// })
